de350b5a280a83237536bfecd244b9f73aa936fb
michalstepan <michalstepan92@gmail.com>
2016-02-15 08:05:21 +0100

    added properties to User class

--- src/java/com/stepan/domain/User.java
+++ src/java/com/stepan/domain/User.java
@@ -4,4 +4,41 @@ package com.stepan.domain;
  * Created by Michal on 15.2.2016.
  */
 public class User {
+
+    private String id;
+    private String username;
+    private String password;
+
+    public User(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
